ce1cae47ad7f75a06143eacf43794e7416ff0235
Adrian Costin <adriancostin6@gmail.com>
2021-10-02 17:46:51 +0300

    Add general header for library

---
+++ include/lbaseconv/baseconv.h
@@ -0,0 +1,15 @@
+#include "lbaseconv/bin2int.h"
+#include "lbaseconv/bin2oct.h"
+#include "lbaseconv/bin2hex.h"
+
+#include "lbaseconv/oct2bin.h"
+#include "lbaseconv/oct2dec.h"
+#include "lbaseconv/oct2hex.h"
+
+#include "lbaseconv/dec2bin.h"
+#include "lbaseconv/dec2oct.h"
+#include "lbaseconv/dec2hex.h"
+
+#include "lbaseconv/hex2bin.h"
+#include "lbaseconv/hex2dec.h"
+#include "lbaseconv/hex2oct.h"
