09ce9fc694cb098b57b31f4475479e7eac8db9f2
Adrian Costin <adriancostin6@gmail.com>
2021-10-25 15:29:27 +0300

    Fix test cases name

--- test/bin2oct.cpp
+++ test/bin2oct.cpp
@@ -4,26 +4,26 @@ extern "C" {
 #include "lbaseconv/baseconv.h"
 }
 
-TEST(bin2int, good_input) {
+TEST(bin2oct, good_input) {
     ASSERT_EQ(btoo("1100", 4), 14);
 }
 
-TEST(bin2int, empty_input) {
+TEST(bin2oct, empty_input) {
     ASSERT_EQ(btoo("", 0), -1);
 }
 
-TEST(bin2int, empty_input_positive_size) {
+TEST(bin2oct, empty_input_positive_size) {
     ASSERT_EQ(btoo("", 10), -1);
 }
 
-TEST(bin2int, null_input) {
+TEST(bin2oct, null_input) {
     ASSERT_EQ(btoo(NULL, 0), -1);
 }
 
-TEST(bin2int, null_input_positive_size) {
+TEST(bin2oct, null_input_positive_size) {
     ASSERT_EQ(btoo(NULL, 10), -1);
 }
 
-TEST(bin2int, long_input) {
+TEST(bin2oct, long_input) {
     ASSERT_EQ(btoo("10101010101010101010", 20), 2525252);
 }
